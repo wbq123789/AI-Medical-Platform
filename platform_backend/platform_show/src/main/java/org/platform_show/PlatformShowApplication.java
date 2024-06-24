3f0ad71d8fb51cbac89498fadafb89e77c417e9c
Flying <1724593655@qq.com>
2024-06-24 13:30:09 +0800

    项目初始化

---
+++ platform_backend/platform_show/src/main/java/org/platform_show/PlatformShowApplication.java
@@ -0,0 +1,13 @@
+package org.platform_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformShowApplication.class, args);
+    }
+
+}
---
+++ platform_backend/platform_show/src/test/java/org/platform_show/PlatformShowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.platform_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
